248fc0d7fe53670dd3c0e18924b03d0cf78fdc18
Aleksandr Baklanov <Aleksandr_Baklanov@epam.com>
2022-01-12 19:30:04 +0500

    switch docker-compose format vesion to 2.4

--- homework/make_all_good.sh
+++ homework/make_all_good.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# deploy local registry
+## deploy local registry
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 
-# create networks
+## create networks
 docker network create -d bridge nginx-network --subnet 172.30.0.0/24
 docker network create -d bridge apache-network --subnet 172.31.0.0/24
 
 
-# build docker images
+## build docker images
 docker build -t homework_nginx -f ./nginx/Dockerfile .
 docker build -t homework_apache -f ./apache/Dockerfile .
 
@@ -31,7 +31,7 @@ docker tag homework_apache localhost:5000/homework_apache:latest
 # localhost:5000/homework_nginx                                     latest              3aaad4b8026b        16 minutes ago      23.5MB
 
 
-# push nginx to the local registry
+## push nginx to the local registry
 docker push localhost:5000/homework_nginx:latest
 
 # The push refers to repository [localhost:5000/homework_nginx]
@@ -44,7 +44,7 @@ docker push localhost:5000/homework_nginx:latest
 # latest: digest: sha256:2518cd4bd59e3c6a510cb155e2d11271afa9dd7b1189855da61dd5ba6205a9d6 size: 1568
 
 
-# push apache to the local registry
+## push apache to the local registry
 docker push localhost:5000/homework_apache
 
 # The push refers to repository [localhost:5000/homework_apache]
@@ -57,5 +57,13 @@ docker push localhost:5000/homework_apache
 # latest: digest: sha256:d6553666502986042b054bfb9e3ad943f5d6c62ae42e4787c73e5c054be93e67 size: 1572
 
 
-# get both apps up with docker-compose
+## get both apps up with docker-compose
 docker-compose up -d
+
+## show lomits
+echo "limits for containers are:"
+echo "NanoCpus for apache_app - $(docker inspect --format '{{json .HostConfig.NanoCpus}}' apache_app | numfmt --to=iec)"
+echo "Memory for apache_app - $(docker inspect --format '{{json .HostConfig.Memory}}' apache_app | numfmt --to=iec)"
+
+echo "NanoCpus for apache_app - $(docker inspect --format '{{json .HostConfig.NanoCpus}}' nginx_app | numfmt --to=iec)"
+echo "Memory for apache_app - $(docker inspect --format '{{json .HostConfig.Memory}}' nginx_app | numfmt --to=iec)"
